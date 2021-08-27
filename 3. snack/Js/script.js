7752480d2381196bc272f671691de8a0dc37d2fa
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-27 13:14:20 +0200

    fix

--- 3. snack/Js/script.js
+++ 3. snack/Js/script.js
@@ -9,7 +9,7 @@ var invitati = [];
 //2. Accoppiare un nome casuale con un cognome casuale => math.random(da 0 a 2) tramite indice array
 for ( var i = 0; i<nomi.length; i++ ) {
 
-    invitati[i] = ( nomi[Math.floor(Math.random() * 2)] + " " + cognomi[Math.floor(Math.random() * 2)]);
+    invitati[i] = ( nomi[Math.floor(Math.random() * 3)] + " " + cognomi[Math.floor(Math.random() * 3)]);
     
     // evitare la ripetizione
     // while ( invitati[i] ==  ) {
