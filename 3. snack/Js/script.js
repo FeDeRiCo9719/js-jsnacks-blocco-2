c409d489e261eee3951383e7175b629752b6efc7
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-27 12:52:32 +0200

    liste

---
+++ 3. snack/Js/script.js
@@ -0,0 +1,8 @@
+/* Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati. */
+
+//1. creare due array => due liste (nomi e cognomi)
+var nomi = ["Marco", "Francesco", "Andrea"];
+var cognomi = ["Rossi", "Brambilla", "Fumagalli"];
+
+//2. Accoppiare un nome casuale con un cognome casuale => math.random(da 0 a 2) tramite indice array
