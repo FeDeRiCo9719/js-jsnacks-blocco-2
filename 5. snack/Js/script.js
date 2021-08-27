e0ce40edb37d747debc5defbfdb44c9197e91125
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-27 16:54:54 +0200

    fix

--- 5. snack/Js/script.js
+++ 5. snack/Js/script.js
@@ -14,7 +14,17 @@ var array_1 = [ 1, 2, 3, 4, 5 ];
 var array_2 = [ 10, 20, 30, 40, 50, 60, 70, 80, 90 ];
 
 // 2. Stabilire il numero di elementi che li compongono
-console.log(array_1.length);
-console.log(array_2.length);
+var length_1 = (array_1.length);
+var length_2 = (array_2.length);
+
+console.log(length_1);
+console.log(length_2);
 
 // 3. confrontare i due valori, e accrescere di elementi casuali il più piccolo fino a pareggiare l'altro
+if ( length_1 > length_2 ) {
+
+} else if ( length_1 < length_2 ) {
+
+} else {
+    
+}
