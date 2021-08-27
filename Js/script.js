0bf42d1fc17117277f928b4b2ab102d6909746b5
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-27 12:07:38 +0200

    Ciclo while

--- Js/script.js
+++ Js/script.js
@@ -3,14 +3,33 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while. */
 
 // 1. propmt * 5 => ciclo * 5
-// 2. stampar la sommare
+// 2. stampare la sommare
 
 //INPUT 
 // 1. propmt * 5 => ciclo * 5
 var somma = 0;
 var supporto = 0;
 
-for ( var i = 0; i<5; i++ ) {
+// for ( var i = 0; i<5; i++ ) {
+//     supporto = parseInt( prompt ("inserisci un numero") );
+//     while (isNaN(supporto)) {
+//         alert("carattere non valido, riprova");
+//         supporto = parseInt(prompt("inserisci un numero casuale"));
+//     }
+//     console.log(supporto);
+//     somma  = somma  + supporto
+// }
+
+// //OUTPUT 
+// // 2. stampare la sommare
+// console.log(somma);
+
+
+// CICLO WHILE
+
+var i = 0;
+
+while (i<5) {
     supporto = parseInt( prompt ("inserisci un numero") );
     while (isNaN(supporto)) {
         alert("carattere non valido, riprova");
@@ -18,8 +37,9 @@ for ( var i = 0; i<5; i++ ) {
     }
     console.log(supporto);
     somma  = somma  + supporto
+    i++
 }
 
 //OUTPUT 
-// 2. stampar la sommare
+// 2. stampare la sommare
 console.log(somma);
