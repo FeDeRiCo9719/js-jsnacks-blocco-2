1e3cfb6605452477e0765352cbac624bf4245f70
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-27 16:31:51 +0200

    fix

--- 4. snack/Js/script.js
+++ 4. snack/Js/script.js
@@ -3,25 +3,17 @@ e fai la somma di tutti gli elementi che sono in posizione (indice dell'array) d
 
 // INPUT - creazione array e variabile somma
 var array = [ 20, 30, 40, 50, 60, 70, 80 ];
-console.log( "INDICE DI 3 = " + array.indexOf(3)); // Estrapolare l'indice di un elemento all'interno di un array
 var somma = 0;
-var indice = [];
+// console.log( "INDICE DI 30 = " + array.indexOf(30)); // Estrapolare l'indice di un elemento all'interno di un array
 
-// PROGRAMMA somma 
+// PROGRAMMA - somma 
 for ( var i = 0; i<array.length; i++ ) {
 
-    console.log(array.indexOf(array));
+    if ( (array.indexOf(array[i])) % 2 == 1 ) {
+        somma = somma + array[i];
+    }
 
-    // indice.push(array.indexOf(20));
-    // console.log( "ciao " + indice);
-
-    // if ( indice % 2 == 0 ) {
-    //     somma = somma + array[i];
-    //     console.log("valore array al giro = " + array[i])
-    //     console.log("valore somma + array al giro = " + somma);
-    //     console.log("---------------------------");
-    // }
 }
 
 //OUTPUT stampare somma totale
-// console.log("somma valore totale = " + somma);
+console.log("somma valore totale = " + somma);
