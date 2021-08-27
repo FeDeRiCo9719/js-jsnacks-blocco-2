ddda0cdbaa70298e97b250dfb1f84c3306b3dfe6
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-27 12:20:41 +0200

    secondo esercizio

---
+++ js-jsnacks-2/Js/script.js
@@ -0,0 +1,11 @@
+//Inserisci un numero, se è pari stampa il numero,
+//se è dispari stampa il numero successivo
+
+//INPUT
+// 1. inserire un numero (variabile)
+var num = parseInt(prompt("Inserisci un numero"));
+
+// 2. SE il numero è pari stampalo
+if (num % 2 == 0) {
+    alert("Il numero " + num + " è pari");
+}
