30421714b0b4853fe206a0d8dd07ebcc6a07d6c3
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-27 12:58:06 +0200

    ciclo for

--- 3. snack/Js/script.js
+++ 3. snack/Js/script.js
@@ -4,5 +4,9 @@ lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati. */
 //1. creare due array => due liste (nomi e cognomi)
 var nomi = ["Marco", "Francesco", "Andrea"];
 var cognomi = ["Rossi", "Brambilla", "Fumagalli"];
+var supporto = [];
 
 //2. Accoppiare un nome casuale con un cognome casuale => math.random(da 0 a 2) tramite indice array
+for ( var i = 0; i<nomi.length; i++ ) {
+    console.log( nomi[Math.floor(Math.random() * 2)] + cognomi[Math.floor(Math.random() * 2)]);
+}
